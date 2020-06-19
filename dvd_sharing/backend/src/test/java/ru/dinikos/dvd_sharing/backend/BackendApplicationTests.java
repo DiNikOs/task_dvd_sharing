dd9cbdb663a95f881ade20b49420b738d41abc58
DiNikOs <demon22odn08@rambler.ru>
2020-06-19 23:38:16 +0300

    Start

---
+++ dvd_sharing/backend/src/test/java/ru/dinikos/dvd_sharing/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dinikos.dvd_sharing.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
